0a2f60db884f7c2699b2b5a805590173cdf65ff8
Mr-X-01 <60137544+Mr-X-01@users.noreply.github.com>
2020-08-03 23:10:21 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt install python
+apt install python3.8
+sudo rm $PREFIX/bin/python && sudo ln -s python3.8 $PREFIX/bin/python
+apt install python-pip
+pip install colorama
+pip install proxyscrape
+pip install requests
+pip install --upgrade pip
